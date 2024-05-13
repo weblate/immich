b7ed1bca7de45a97c1a552d0957c104e107dcfdb
martabal <74269598+martabal@users.noreply.github.com>
2024-05-13 23:13:57 +0200

    fix: new feature photo

--- web/src/lib/components/faces-page/people-card.svelte
+++ web/src/lib/components/faces-page/people-card.svelte
@@ -19,6 +19,7 @@
 
   export let person: PersonResponseDto;
   export let preload = false;
+  export let now: number | undefined = undefined;
 
   type MenuItemEvent = 'change-name' | 'set-birth-date' | 'merge-people' | 'hide-person';
   let dispatch = createEventDispatcher<{
@@ -56,7 +57,7 @@
       <ImageThumbnail
         shadow
         {preload}
-        url={getPeopleThumbnailUrl(person.id)}
+        url={`${getPeopleThumbnailUrl(person.id)}${now ? `?now=${now}` : ''}`}
         altText={person.name}
         title={person.name}
         widthStyle="100%"
